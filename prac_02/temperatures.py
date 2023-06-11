2dc0f7b0a828a60f80a51c7ef1a6d05280f19d1b
amistlr <icandrew08@gmail.com>
2023-06-11 19:37:00 +1000

    Refactor conversion formula, and display result

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -8,20 +8,45 @@ F - Convert Fahrenheit to Celsius
 Q - Quit"""
 print(MENU)
 choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print(f"Result: {fahrenheit:.2f} F")
-    elif choice == "F":
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print(f"Result {celsius:2f} C")
-        # TODO: Write this section to convert F to C and display the result
-        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
-        # Remove the "pass" statement when you are done. It's a placeholder.
-    else:
-        print("Invalid option")
-    print(MENU)
-    choice = input(">>> ").upper()
+
+
+def main():
+    global choice
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = calculate_fahrenheit(celsius)
+            print_fahrenheit(fahrenheit)
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = calculate_celsius(celsius, fahrenheit)
+            print_celsius(celsius)
+            # TODO: Write this section to convert F to C and display the result
+            # Hint: celsius = 5 / 9 * (fahrenheit - 32)
+            # Remove the "pass" statement when you are done. It's a placeholder.
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+
+
+def print_celsius(celsius):
+    print(f"Result {celsius:2f} C")
+
+
+def calculate_celsius(celsius, fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
+def print_fahrenheit(fahrenheit):
+    print(f"Result: {fahrenheit:.2f} F")
+
+
+def calculate_fahrenheit(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
+main()
 print("Thank you.")
