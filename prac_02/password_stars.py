55be6e501b8dc3dedcc644ae52e1308ebab55e97
amistlr <icandrew08@gmail.com>
2023-06-11 19:09:42 +1000

    Refactor to separate functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,20 @@
-password_length = 0
+def main():
+    password_length = 0
 
-while password_length < 6:
-    password = input("Password: ")
-    password_length = len(password)
+    password_length = get_name(password_length)
 
-print("*" * password_length)
+    print_password(password_length)
+
+
+def print_password(password_length):
+    print("*" * password_length)
+
+
+def get_name(password_length):
+    while password_length < 6:
+        password = input("Password: ")
+        password_length = len(password)
+    return password_length
+
+
+main()
