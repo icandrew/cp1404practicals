c030fc3d43b1cb88a5696db54f895bb6f61ff9ba
amistlr <icandrew08@gmail.com>
2023-06-18 00:04:04 +1000

    Added number_of_days variable and update the display

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -14,9 +14,10 @@ MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 0.01
 MAX_PRICE = 1000.0
 INITIAL_PRICE = 10.0
+number_of_days = 0  # day count
 
 price = INITIAL_PRICE
-print(f"${price:,.2f}")
+print(f"Starting price: ${price:,.2f}")  # updated display based from Things To Do
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
@@ -32,4 +33,5 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"${price:,.2f}")
+    number_of_days += 1  # day increments
+    print(f"On day {number_of_days} price is: ${price:,.2f}")  # update display and added day count
