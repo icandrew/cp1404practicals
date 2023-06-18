718a2f58620ba84dce09f78942d55027ce3a75dd
amistlr <icandrew08@gmail.com>
2023-06-18 18:19:13 +1000

    Puts the code in a function

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,6 +1,13 @@
-FILENAME = "name.txt"
+def main():
+    filename = "name.txt"
+    get_name(filename)
 
-out_file = open("name.txt", "w")
-name = str(input("Enter Name: "))  # Ask user to input a name
-print(name, file=out_file)  # Print name
-out_file.close()
+
+def get_name(filename):
+    out_file = open(filename, "w")
+    name = str(input("Enter Name: "))  # Ask user to input a name
+    print(name, file=out_file)  # Print name
+    out_file.close()
+
+
+main()
