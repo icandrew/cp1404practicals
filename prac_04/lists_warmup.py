24f7ee13f158e4dca86630cba23d613e45d10425
amistlr <icandrew08@gmail.com>
2023-06-23 17:09:17 +1000

    Wrote statements to achieve the requirements

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,16 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+#  numbers[0] = 3
+#  numbers[-1] = 2
+#  numbers[3] = 1
+#  numbers[:-1] = [3, 1, 4, 1, 5, 9]
+#  numbers[3:4] = [1, 9]
+#  5 in numbers = True
+#  7 in numbers = False
+#  "3" in numbers = False
+#  numbers + [6, 5, 3] = [9, 6, 7, 5, 9, 2]
+
+numbers[0] = "ten"
+numbers[6] = 1
+numbers[2:6]
+9 in numbers
