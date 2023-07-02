d54b0ed812e84e1e41d823a6d7f47a76c303a20c
amistlr <icandrew08@gmail.com>
2023-07-02 14:07:11 +1000

    Store name and email in dictionary

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,12 +1,14 @@
 def main():
+    email_to_name = {}
     email = input("Email: ")
     while email != "":
         name = extract_name(email)
         verification = input(f"Is your name {name}? (Y/n) ")
         if verification.upper() != "Y" and verification != "":
             name = input("Name: ")
-            print(name)
+        email_to_name[name] = email
         email = input("Email: ")
+    print(email_to_name)
 
 
 def extract_name(email):
