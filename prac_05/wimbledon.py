4743e3d8264df85e7601a83a9931fbb48493a135
amistlr <icandrew08@gmail.com>
2023-07-02 15:48:00 +1000

    Get the index of country and champion

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -2,4 +2,5 @@ with open("wimbledon.csv", "r", encoding="utf-8-sig") as in_file:
     in_file.readline()
     for line in in_file:
         data = line.strip().split(",")
-        print(data)
+        print(f"{data[1]}, {data[2]}")
+
