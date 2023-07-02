918c1159f86d4cd17b9038d72b4b654b0d2c57d6
amistlr <icandrew08@gmail.com>
2023-07-02 13:29:51 +1000

    Create while loop to ask user email and verify the name

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,5 +1,14 @@
-email = input("Email: ")
-parts = email.split("@")[0]
-name_parts = parts.split(".")
-name = " ".join(name_parts)
-print(name)
+def main():
+    email = input("Email: ")
+    while email != "":
+        parts = email.split("@")[0]
+        name_parts = parts.split(".")
+        name = " ".join(name_parts)
+        verification = input(f"Is your name {name}? (Y/n) ")
+        if verification.upper() != "Y" and verification != "":
+            name = input("Name: ")
+            print(name)
+        email = input("Email: ")
+
+
+main()
