9f69ed7c43b6032aab0c8bedb467b6cf58ec9318
amistlr <icandrew08@gmail.com>
2023-07-02 15:28:50 +1000

    Skip the header and strip each line

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,3 +1,4 @@
 with open("wimbledon.csv", "r", encoding="utf-8-sig") as in_file:
-    lines = in_file.readlines()
+    in_file.readline()
+    lines = [line.strip() for line in in_file.readlines()]
     print(lines)
