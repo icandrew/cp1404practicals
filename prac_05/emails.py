1a64a6feb53efa462bc29ba892281c5abd24a0d1
amistlr <icandrew08@gmail.com>
2023-07-02 13:30:43 +1000

    Refactor to extract the name from email

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,14 +1,19 @@
 def main():
     email = input("Email: ")
     while email != "":
-        parts = email.split("@")[0]
-        name_parts = parts.split(".")
-        name = " ".join(name_parts)
+        name = extract_name(email)
         verification = input(f"Is your name {name}? (Y/n) ")
         if verification.upper() != "Y" and verification != "":
             name = input("Name: ")
             print(name)
         email = input("Email: ")
+
+
+def extract_name(email):
+    parts = email.split("@")[0]
+    name_parts = parts.split(".")
+    name = " ".join(name_parts)
+    return name
 
 
 main()
