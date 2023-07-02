ee359cca36fa610a72d31ccd5a01786b54c8cb15
amistlr <icandrew08@gmail.com>
2023-07-02 10:12:55 +1000

    Align the output in a nice column

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -5,7 +5,8 @@ for word in words:
     word_to_count[word] = word_to_count.get(word, 0) + 1
 words = list(word_to_count.keys())
 words.sort()
+max_character = max(len(word) for word in list(word_to_count.keys()))
 for word in words:
-    print(f"{word} - {word_to_count[word]}")
+    print(f"{word:{max_character}} - {word_to_count[word]}")
 
 
