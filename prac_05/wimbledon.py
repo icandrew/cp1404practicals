d68c0f82d10b74e1012b2ff45e0b5215af817679
amistlr <icandrew08@gmail.com>
2023-07-02 15:41:09 +1000

    Strip and split each line

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,4 +1,5 @@
 with open("wimbledon.csv", "r", encoding="utf-8-sig") as in_file:
     in_file.readline()
-    lines = [line.strip() for line in in_file.readlines()]
-    print(lines)
+    for line in in_file:
+        data = line.strip().split(",")
+        print(data)
