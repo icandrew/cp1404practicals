18a90c3a3e05108763fe7dd3448dd9e22250c2fd
amistlr <icandrew08@gmail.com>
2023-07-02 10:10:19 +1000

    Sort the output

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -4,6 +4,7 @@ words = text.split()
 for word in words:
     word_to_count[word] = word_to_count.get(word, 0) + 1
 words = list(word_to_count.keys())
+words.sort()
 for word in words:
     print(f"{word} - {word_to_count[word]}")
 
