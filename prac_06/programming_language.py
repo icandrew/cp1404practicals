76ebc5c6b6eec8f3331f69d594cbd35f3e00005c
amistlr <icandrew08@gmail.com>
2023-07-20 15:17:19 +1000

    Create class ProgrammingLanguage, set __init__ parameters

---
+++ prac_06/programming_language.py
@@ -0,0 +1,14 @@
+class ProgrammingLanguage:
+    def __init__(self, name="", typing="Dynamic", reflection="Yes", year=0):
+        #  set default for typing and reflection
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+
+
+
+
+
+
