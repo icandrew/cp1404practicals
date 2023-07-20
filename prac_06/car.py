954b4612ec9522f59178a3a3e2e8ffe79a317b90
amistlr <icandrew08@gmail.com>
2023-07-20 14:50:40 +1000

    Add __str__ method to the Class car

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,13 +4,16 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, fuel=0, odometer=0):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
         """
         self.fuel = fuel
-        self._odometer = 0
+        self._odometer = odometer
+
+    def __str__(self):
+        return f"Car, fuel={self.fuel}, odometer={self._odometer}"
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
