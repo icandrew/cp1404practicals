77201fedf4ae4d9994a6de1b82cfc03fb0c33888
amistlr <icandrew08@gmail.com>
2023-07-20 15:19:25 +1000

    Import programming_language.py and copy the code

---
+++ prac_06/languages.py
@@ -0,0 +1,16 @@
+"""
+CP1404 Practical 06 - Programming Language
+Time to complete -
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+
+
+main()
