81a722cbbf2b6073580cb1cedc38caf4c8e0b142
amistlr <icandrew08@gmail.com>
2023-07-20 18:09:18 +1000

    Comment the input function and test program

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -3,16 +3,19 @@ vintage_year = 50
 
 
 class Guitar:
-    def __int__(self, name="", year=0, cost=0.00):
+    def __init__(self, name="", year=0, cost=0.00):
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
-        return f"{self.name}({self.year}) : {self.cost}"
+        return f"{self.name}({self.year}) : {self.cost:.2f}"
 
     def get_age(self):
         return current_year - self.year
+
+    def __lt__(self, other):
+        return self.year < other.year
 
     def is_vintage(self):
         return self.get_age() >= vintage_year
