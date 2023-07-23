7557c5168b7ca847fbcc7ce7be77d3f771df77ba
amistlr <icandrew08@gmail.com>
2023-07-23 17:11:18 +1000

    Create a program that read data in guitar.csv

---
+++ prac_07/guitar_file_reader.py
@@ -0,0 +1,16 @@
+from guitar import Guitar
+
+
+def main():
+    guitars = []
+    with open("guitars.csv", "r") as in_file:
+        in_file.readline()
+        for line in in_file:
+            parts = line.strip().split(',')
+            guitar = Guitar(parts[0], parts[1], parts[2])
+            guitars.append(guitar)
+        for guitar in guitars:
+            print(guitar)
+
+
+main()
