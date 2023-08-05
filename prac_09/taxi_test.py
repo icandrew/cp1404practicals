ee95d341dfa41d0d31e11b322364ed047d72c015
amistlr <icandrew08@gmail.com>
2023-08-05 21:20:20 +1000

    Restart taxi meter and drive 100 km, display taxi details and current fare

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -9,6 +9,9 @@ def main():
     my_taxi = Taxi(name="Prius 1", fuel=100, price_per_km=1.23)
     my_taxi.drive(40)
     print(f"{my_taxi}\nCurrent fare: ${my_taxi.get_fare():.2f}")
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(f"{my_taxi}\nCurrent fare: ${my_taxi.get_fare():.2f}")
 
 
 main()
