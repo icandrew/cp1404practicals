e6f9d07e04a9e1781520e5c9d695ae8b4beed019
amistlr <icandrew08@gmail.com>
2023-08-05 23:07:54 +1000

    Test to check if UnreliableCar class is working

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -2,3 +2,14 @@
 CP1404 - Programming II Practical 09
 UnreliableCar Test
 """
+
+from unreliable_car import UnreliableCar
+
+
+def main():
+    test_car = UnreliableCar(name="Nissan Skyline", reliability=60.50)
+    test_car.drive(50)
+    print(test_car)
+
+
+main()
