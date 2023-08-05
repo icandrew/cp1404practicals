570c9e78a7b99a532aa2710167640ed4b0979331
amistlr <icandrew08@gmail.com>
2023-08-05 21:14:30 +1000

    Create new taxi object my_taxi "Prius 1" with 100 units of fuel and fare 1.23 per_km

--- prac_09/test_taxi.py
+++ prac_09/test_taxi.py
@@ -2,3 +2,12 @@
 CP1404 - Programming II Practical 09
 Test Taxi
 """
+from taxi import Taxi
+
+
+def main():
+    my_taxi = Taxi(name="Prius 1", fuel=100, price_per_km=1.23)
+    print(f"{my_taxi}")
+
+
+main()
