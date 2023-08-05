a3a6b166d3dff34cb90a2700c1cbc6f095bf04a5
amistlr <icandrew08@gmail.com>
2023-08-05 22:30:39 +1000

    Create UnreliableCar class that inherits from Car

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -2,3 +2,16 @@
 CP1404 - Programming II Practical 09
 UnreliableCar class
 """
+from car import Car
+
+
+class UnreliableCar(Car):
+    """Initialize UnreliableCar, based on parent class Car."""
+    def __init__(self, reliability=float, **kwargs):
+        super().__init__(**kwargs)
+        self.reliability = reliability
+
+    def __str__(self):
+        return f"{super().__str__()}, {self.reliability} reliable"
+
+
