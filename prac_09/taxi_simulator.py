5c901efe6d8012197f74c784c19cb5702f8ccf8f
amistlr <icandrew08@gmail.com>
2023-08-06 22:56:29 +1000

    Add and test display_taxi function

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -2,11 +2,13 @@
 CP1404 - Programming II Practical 09
 Taxi Simulator
 """
-from taxi import Taxi
 from car import Car
+from taxi import Taxi
 from silver_service_taxi import SilverServiceTaxi
 
 MENU = "q)uit, c)hoose taxi, d)rive"
+taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+bill_to_date = 0
 
 
 def main():
@@ -15,9 +17,16 @@ def main():
     choice = input(">>> ").lower()
     while choice != "q":
         if choice == "c":
-            pass
+            print("Taxis available: ")
+            display_taxis(taxis)
+            break
         elif choice == "d":
             pass
+
+
+def display_taxis(taxis):
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
 
 
 main()
