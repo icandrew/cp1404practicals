c1600cfcc5d52a2a5b784cd33f268443f327920a
amistlr <icandrew08@gmail.com>
2023-08-06 14:37:48 +1000

    Create new class SilverServiceTaxi that inherits from Taxi class, add new attribute fanciness which is a float, and pass fanciness multiplied by self.price_per_km

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -2,3 +2,13 @@
 CP1404 - Programming II Practical 09
 Silver Service Taxi Class
 """
+from taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """Initialize SilverServiceTaxi, that inherits from Taxi."""
+
+    def __init__(self, fanciness=float, **kwargs):
+        super().__init__(**kwargs)
+        self.fanciness = fanciness
+        self.price_per_km *= fanciness
