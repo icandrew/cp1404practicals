6b3a2691158d69fddb6fb7918b9608a32f45473a
amistlr <icandrew08@gmail.com>
2023-08-06 15:40:39 +1000

    Add get_fare function to return the price of taxi plus the flagfall

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -11,8 +11,14 @@ class SilverServiceTaxi(Taxi):
 
     def __init__(self, name="", fuel=0, fanciness=float):
         super().__init__(name, fuel)
+
         self.fanciness = fanciness
         self.price_per_km *= fanciness
 
     def __str__(self):
+        """Return the string display of SilverServiceTaxi"""
         return f"{super().__str__()}, plus flagfall of ${self.flagfall:.2f}"
+
+    def get_fare(self):
+        """Return the price for the taxi trip plus the flagfall."""
+        return super().get_fare() + self.flagfall
