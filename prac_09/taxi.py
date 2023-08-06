a6d147fcd135059799a3bc5b6624355d2808752c
amistlr <icandrew08@gmail.com>
2023-08-06 22:54:15 +1000

    Update __init__ with name, fuel

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -2,16 +2,17 @@
 CP1404/CP5632 Practical
 Car class
 """
-from prac_09.car import Car
+from car import Car
 
 
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
+
     price_per_km = 1.23
 
-    def __init__(self, **kwargs):
+    def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
-        super().__init__(**kwargs)
+        super().__init__(name, fuel)
         self.current_fare_distance = 0
 
     def __str__(self):
