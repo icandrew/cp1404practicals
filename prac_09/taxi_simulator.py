659f619e1549a0eecfad0a4dba01d7a3b07bf4c3
amistlr <icandrew08@gmail.com>
2023-08-06 23:36:40 +1000

    Add select_taxi function, handles ValueError and invalid taxi choice

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -20,8 +20,12 @@ def main():
         if choice == "c":
             print("Taxis available: ")
             display_taxis(taxis)
+            current_taxi = select_taxi(taxis)
         elif choice == "d":
-            pass
+            if current_taxi is None:
+                print("You need to choose a taxi before you can drive")
+            else:
+                pass
         else:
             print("Invalid option")
             break
@@ -33,6 +37,17 @@ def main():
 def display_taxis(taxis):
     for i, taxi in enumerate(taxis):
         print(f"{i} - {taxi}")
+
+
+def select_taxi(taxis):
+    try:
+        selected_taxi = int(input("Choose Taxi:"))
+        if 1 <= selected_taxi <= len(taxis):
+            return taxis[selected_taxi - 1]
+        else:
+            print("Invalid taxi choice")
+    except ValueError:
+        print(f"Choice must be a number  not more than {len(taxis)}")
 
 
 main()
