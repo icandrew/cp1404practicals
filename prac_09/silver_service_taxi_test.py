078b8025e3208e3a533dc6cdd47a7c4faf4f7112
amistlr <icandrew08@gmail.com>
2023-08-06 15:02:12 +1000

    Test SilverServiceTaxi class

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -2,3 +2,14 @@
 CP1404 - Programming II Practical 09
 Silver Service Taxi Test
 """
+
+from silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    test_car = SilverServiceTaxi(name="Hummer", fuel=200, fanciness=4)
+    test_car.drive(0)
+    print(test_car)
+
+
+main()
