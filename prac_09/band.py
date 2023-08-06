34fb765ace6e1900911567ec98ec3a0cfc6f97df
amistlr <icandrew08@gmail.com>
2023-08-06 22:03:23 +1000

    Update add function, add musician to the Band

--- prac_09/band.py
+++ prac_09/band.py
@@ -3,13 +3,14 @@ CP1404 - Programming II Practical 09
 Band Class
 """
 
+
 class Band:
     def __init__(self, name=""):
         self.name = name
         self.musicians = []
 
-    def add(self):
-        pass
+    def add(self, musician):
+        self.musicians.append(musician)
 
     def play(self):
         pass
