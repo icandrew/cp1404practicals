d94338e90f1b8487b2c1fe0ae5ced6bee5b80432
amistlr <icandrew08@gmail.com>
2023-08-06 22:19:45 +1000

    Update docstrings

--- prac_09/band.py
+++ prac_09/band.py
@@ -6,13 +6,16 @@ Band Class
 
 class Band:
     def __init__(self, name=""):
+        """Initialize Band, with list of musicians"""
         self.name = name
         self.musicians = []
 
     def add(self, musician):
+        """Adds musician to a band"""
         self.musicians.append(musician)
 
     def play(self):
+        """Return a string that identify musicians with/without instrument in the band"""
         active_musician = ""
         for musician in self.musicians:
             if not musician.instruments:
@@ -20,8 +23,8 @@ class Band:
             else:
                 active_musician += f"{musician.name} is playing {musician.instruments[0]}\n"
         return active_musician
-        
 
     def __str__(self):
+        """Return a string represenation of the Band and its members"""
         musicians_str = ", ".join([musician.name for musician in self.musicians])
         return f"Band: {self.name}, Members: {musicians_str}"
