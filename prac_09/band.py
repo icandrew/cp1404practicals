f94bff768f8e1d6f3d236979b21c715c5e2e7fb8
amistlr <icandrew08@gmail.com>
2023-08-06 21:25:29 +1000

    Initial commit band.py

---
+++ prac_09/band.py
@@ -0,0 +1,18 @@
+"""
+CP1404 - Programming II Practical 09
+Band Class
+"""
+
+class Band:
+    def __init__(self, name=""):
+        self.name = name
+        pass
+
+    def add(self):
+        pass
+
+    def play(self):
+        pass
+
+    def __str__(self):
+        return f"Band: {self.name}"
