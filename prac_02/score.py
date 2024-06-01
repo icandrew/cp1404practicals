12b8b052bac3ca067876d77fa2f7ac19347e315b
Jake <andrewjake.bagsit@my.jcu.edu.au>
2024-06-01 11:06:17 +1000

    Import randint generates a random score and prints the result

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,14 +1,31 @@
 """
-CP1404/CP5632 - Practical
-Broken program to determine score status
+Refactor broken_score.py to use function
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+from random import randint
+
+
+def main():
+    """Takes user input and a random number for a score, evaluates the score, and prints the result."""
+    user_score = int(input("Enter score: "))
+    mark = evaluate_mark(user_score)
+    print(f"Your score of {user_score} is marked: {mark}")
+
+    random_score = randint(1, 100)
+    random_mark = evaluate_mark(random_score)
+    print(f"Random score of {random_score} is marked: {random_mark}")
+
+
+def evaluate_mark(score):
+    """Evaluates the marks based on the provided score"""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
