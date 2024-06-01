53f1992dc9a8c08c0cbdc2af32eca0a1a6afeaae
Jake <andrewjake.bagsit@my.jcu.edu.au>
2024-06-01 11:14:09 +1000

    Create and test menu

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,3 +1,28 @@
 """
 CP1404 Prac_02 - Score Menu
 """
+
+MENU = """(G)et a valid score
+(P)rint result
+(S)how stars
+(Q)uit"""
+
+
+def main():
+    print(MENU)
+    choice = input(">>> ")
+    while choice.upper() != "Q":
+        if choice.upper() == "G":
+            print("G option")
+        elif choice.upper() == "P":
+            print("P option")
+        elif choice.upper() == "S":
+            print("P option")
+        else:
+            print("Invalid choice")
+        print(MENU)
+        choice = input(">>> ")
+    print("farewell")
+
+
+main()
